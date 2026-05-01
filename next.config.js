295ebf96b8de3f9a68336344d7e7f2b02c004066
6punto29 <6punto29@gmail.com>
2026-05-01 12:16:25 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint:     { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  serverExternalPackages: ['@supabase/supabase-js', '@supabase/ssr'],
-};
-
-module.exports = nextConfig;
