97d730286e0819cdf37e43bbbc939fc0ea426ede
6punto29 <6punto29@gmail.com>
2026-05-12 23:44:21 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: false },
-  serverExternalPackages: ['@supabase/supabase-js', '@supabase/ssr'],
-};
-
-module.exports = nextConfig;
